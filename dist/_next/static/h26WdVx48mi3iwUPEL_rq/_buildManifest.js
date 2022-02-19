e583fc047af2d21a4df3a98000454357cdc24ab7
Koichi Yamamoto <koichi.yamamoto@smartnews.com>
2022-02-20 02:01:07 +0900

    add dist dir

---
+++ dist/_next/static/h26WdVx48mi3iwUPEL_rq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-724a5506e7570cf3.js"],"/_error":["static/chunks/pages/_error-0a87c8479a04425c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
